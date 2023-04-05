f877222859c7ac78c4ff01a8bc60954ac58ae9ef
Vikramsingh Rajput <vikramsinghdrajput@gmail.com>
2023-04-05 10:41:33 +0530

    Added unique constraint and started cityRepository Added functions to city repository

---
+++ src/repository/city-repository.js
@@ -0,0 +1,58 @@
+const { City } = require("../models/index");
+
+class CityRepository {
+  async createCity({ name }) {
+    try {
+      const city = await City.create({ name });
+      return city;
+    } catch (error) {
+      console.log("something went wrong in repository layer");
+      throw { error };
+    }
+  }
+
+  async deleteCity(cityId) {
+    try {
+      await City.destroy({
+        where: {
+          id: cityId,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.log("something went wrong in repository layer");
+      throw { errow };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    // data = { name : "New Delhi"}
+    try {
+      const city = await City.update(
+        {
+          name: data.name,
+        },
+        {
+          where: {
+            id: cityId,
+          },
+        }
+      );
+    } catch (error) {
+      console.log("something went wrong in repository layer");
+      throw { errow };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("something went wrong in repository layer");
+      throw { errow };
+    }
+  }
+}
+
+module.exports = CityRepository;
